95611c0cfe492e9704c577ec1259d1f29e2aa20a
Rahil <rahil.ankhad@spit.ac.in>
2023-11-27 10:37:27 +0530

    Final Commit

--- src/calculator/divide.js
+++ src/calculator/divide.js
@@ -1,3 +1,13 @@
 /**
  * Division
  */
+
+function divide(firstNum, secondNum) {
+    if(secondNum == 0){
+        throw new Error("Division Not Possible");
+    }
+    return firstNum / secondNum;
+  }
+  
+  module.exports = divide
+  
